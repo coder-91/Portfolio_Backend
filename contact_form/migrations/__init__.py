ff54573f3bc0e15975fb8b9e5d6957deace8777f
VeyselKaraali <veysel-karaali@outlook.de>
2024-11-26 10:40:06 +0100

    Disable migrations for contact_form

---
+++ contact_form/migrations/__init__.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    """
+    Empty Migration Class: Blocks migrations for this app.
+    """
+    dependencies = []
+    operations = []
